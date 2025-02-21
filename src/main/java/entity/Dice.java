e57a1483bf0f082dea164ad431fdff8ab6c1055d
Thea Lønvik <tmal339@gmail.com>
2025-02-21 11:19:43 +0100

    Made the dice class take a list of dice

--- src/main/java/entity/Dice.java
+++ src/main/java/entity/Dice.java
@@ -1,39 +1,47 @@
 package entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dice extends Die {
-  private Die die1;
-  private Die die2;
+
+  private List<Die> dice;
   private int sum;
 
-    public Dice() {
-    this(6);
+  public Dice(int numberOfDice, int sides) {
+    super(sides);
+    dice = new ArrayList<>();
+    for (int i = 0; i < numberOfDice; i++) {
+      dice.add(new Die(sides));
     }
+    this.sum = 0;
+  }
 
-    public Dice(int sides) {
-        this.die1 = new Die(sides);
-        this.die2 = new Die(sides);
-        this.sum = 0;
+  public int rollDice() {
+    sum = 0;
+    for (Die die : dice) {
+      sum += die.rollDie();
     }
+    return sum;
+  }
 
-    public int rollDice() {
-      this.sum = die1.rollDie() + die2.rollDie();
-        return this.sum;
-    }
+  public int getSum() {
+    return sum;
+  }
 
-    public int getSum() {
-        return this.sum;
+  public List<Integer> getDiceValues() {
+    List<Integer> values = new ArrayList<>();
+    for (Die die : dice) {
+      values.add(die.getValue());
     }
+    return values;
+  }
 
-    public int getDie1Value() {
-        return die1.getValue();
+  @Override
+  public void setSides(int sides) {
+    super.setSides(sides);
+    for (Die die : dice) {
+      die.setSides(sides);
     }
-
-    public int getDie2Value() {
-        return die2.getValue();
-    }
-
-    public void setSides(int sides) {
-        die1.setSides(sides);
-        die2.setSides(sides);
-    }
+  }
 }
