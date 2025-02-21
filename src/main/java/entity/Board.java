f46c0dbaef6f26ea07742326b1b0e1dfd46f78c4
Thea Lønvik <tmal339@gmail.com>
2025-02-21 12:35:14 +0100

    Added methods to class 'board'

--- src/main/java/entity/Board.java
+++ src/main/java/entity/Board.java
@@ -1,5 +1,21 @@
 package entity;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Board {
+  private Map<Integer, Tile> tiles;
+
+  public Board() {
+    tiles = new HashMap<>();
+  }
+
+  public void addTile(Tile tile) {
+    tiles.put(tile.getTileId(), tile);
+  }
+
+  public Tile getTile(int tileId) {
+    return tiles.get(tileId);
+  }
 
 }
