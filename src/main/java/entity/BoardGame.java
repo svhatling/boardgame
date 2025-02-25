76a41377a67c569b4d36cf6f69bc54ca27614825
Thea Lønvik <tmal339@gmail.com>
2025-02-25 20:02:58 +0100

    Made some improvements to 'BoardGame' class

--- src/main/java/entity/BoardGame.java
+++ src/main/java/entity/BoardGame.java
@@ -1,5 +1,8 @@
 package entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BoardGame {
 
   private Board board;
@@ -7,51 +10,45 @@ public class BoardGame {
   private Dice dice;
   private Player currentPlayer;
 
-  // Constructor
   public BoardGame() {
     players = new ArrayList<>();
   }
 
-  // Legg til en spiller
+  public BoardGame(Board board, List<Player> players, Dice dice) {
+    this.board = board;
+    this.players = players;
+    this.dice = dice;
+  }
+
   public void addPlayer(Player player) {
     players.add(player);
   }
 
-  // Sett opp brettet
-  public void createBoard() {
+  public void createBoard(int numberOfTiles) {
     board = new Board();
-    for (int i = 1; i <= 100; i++) { // Eksempel: 100 felt
+    for (int i = 1; i <= numberOfTiles; i++) {
       Tile tile = new Tile(i);
       board.addTile(tile);
     }
-
-    // Eksempel på en stige (ladder action) mellom felt 5 og 20
-    Tile startTile = board.getTile(5);
-    Tile endTile = board.getTile(20);
-    startTile.setLandAction(new LadderAction(20, "Stige til felt 20"));
   }
 
-  // Sett opp terningene
   public void createDice() {
-    dice = new Dice(1); // Eksempel: 1 terning
+    dice = new Dice(2, 6);
   }
 
-  // Spill selve spillet
   public void play() {
     boolean gameOver = false;
 
     while (!gameOver) {
       for (Player player : players) {
         currentPlayer = player;
-        int diceValue = dice.roll();
-        System.out.println(player.getName() + " trillet " + diceValue);
+        int diceValue = dice.rollDice();
+        System.out.println(player.getName() + " rolled " + diceValue);
 
-        // Flytt spilleren
         player.move(diceValue);
 
-        // Sjekk om spilleren har vunnet
-        if (player.getCurrentTile().getTileId() == 100) { // Eksempel: Mål er felt 100
-          System.out.println(player.getName() + " har vunnet spillet!");
+        if (player.getCurrentTile().getTileId() == 100) {
+          System.out.println(player.getName() + " has won the game!");
           gameOver = true;
           break;
         }
@@ -59,10 +56,9 @@ public class BoardGame {
     }
   }
 
-  // Hent vinneren (kan være nyttig for sluttlogikk)
   public Player getWinner() {
     for (Player player : players) {
-      if (player.getCurrentTile().getTileId() == 100) { // Eksempel: Mål er felt 100
+      if (player.getCurrentTile().getTileId() == 100) {
         return player;
       }
     }
