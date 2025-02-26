ff2fd960b59cca2e7dbb1d31f77f54fcc3e433d4
sirivha <sirivha@stud.ntnu.no>
2025-02-26 12:22:40 +0100

    Changed landAction to tileAction Removed nextTile for now.

--- src/main/java/entity/Tile.java
+++ src/main/java/entity/Tile.java
@@ -2,35 +2,27 @@ package entity;
 
 public class Tile {
   private int tileId;
-  private Tile nextTile;
-  private TileAction landAction;
+  private TileAction tileAction;
 
   public Tile(int tileId) {
     this.tileId = tileId;
-    this.nextTile = null;
-    this.landAction = null;
   }
 
   public int getTileId() {
     return tileId;
   }
 
-  public Tile getNextTile() {
-    return nextTile;
+  public TileAction getTileAction() {
+    return tileAction;
   }
 
-  public void setNextTile(Tile nextTile) {
-    this.nextTile = nextTile;
+  public void setTileAction(TileAction action) {
+    this.tileAction = action;
   }
 
-  public void setLandAction(TileAction action){
-    this.landAction = action;
-  }
-
-  public void landPlayer(Player player){
-    if(landAction != null){
-      landAction.perform(player);
+  public void landPlayer(Player player) {
+    if (tileAction != null) {
+      tileAction.perform(player);
     }
   }
-
 }
