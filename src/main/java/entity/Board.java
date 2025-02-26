1d65880d409ee7097d273b2253c146b9373722d7
sirivha <sirivha@stud.ntnu.no>
2025-02-26 12:17:37 +0100

    Added getBoard method

--- src/main/java/entity/Board.java
+++ src/main/java/entity/Board.java
@@ -10,12 +10,15 @@ public class Board {
     tiles = new HashMap<>();
   }
 
+  public Map<Integer, Tile> getBoard() {
+    return tiles;
+  }
+
   public void addTile(Tile tile) {
     tiles.put(tile.getTileId(), tile);
   }
 
   public Tile getTile(int tileId) {
-    return tiles.get(tileId);
+    return tiles.getOrDefault(tileId, new Tile(90));
   }
-
 }
