bac2ae11c7c90fa8033d2aeb53fb88a8c568f14c
sirivha <sirivha@stud.ntnu.no>
2025-02-26 12:21:44 +0100

    Added board in player class, and changed player constructor to include board (may remove if we find a better way), this was to avoid board being null when player is made)
    Added getname method, getcurrenttile, getboard and added some to the move method

--- src/main/java/entity/Player.java
+++ src/main/java/entity/Player.java
@@ -1,26 +1,46 @@
 package entity;
 
 public class Player {
-  private String playerName;
+  private String name;
   private Tile currentTile;
+  private Board board;
 
-  public Player(String playerName, Tile currentTile) {
-    this.playerName = playerName;
-    this.currentTile = null;
+  // Legg til konstruktør for å sette navn, startfelt og board
+  public Player(String name, Tile startTile, Board board) {
+    this.name = name;
+    this.currentTile = startTile;
+    this.board = board;
+  }
+
+  // Metode for å hente spillerens navn
+  public String getName() {
+    return name;
   }
 
   public void placeOnTile(Tile tile) {
     this.currentTile = tile;
   }
 
-  public void move(int steps){
-    for (int i = 0; i < steps; i++) {
-      if (currentTile.getNextTile() != null){
-        currentTile = currentTile.getNextTile();
-      }else{
-        break;
-      }
-    }
+  public Tile getCurrentTile() {
+    return this.currentTile;
   }
 
+  public Board getBoard() {
+    return this.board;
+  }
+
+  public void move(int roll, Board board) {
+    int currentTileId = this.currentTile.getTileId();
+    int targetTileId = currentTileId + roll;
+
+    // Finn det nye målfeltet
+    Tile targetTile = board.getTile(targetTileId);
+
+    // Hvis det er en TileAction på målfeltet, utfør handlingen (for eksempel stige)
+    if (targetTile.getTileAction() != null) {
+      targetTile.getTileAction().perform(this);  // Kun send Player til perform-metoden
+    } else {
+      this.placeOnTile(targetTile);
+    }
+  }
 }
