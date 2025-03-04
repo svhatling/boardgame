5c72423cc92430842ef1635e9b52ced7a04dac76
sirivha <sirivha@stud.ntnu.no>
2025-03-04 13:10:10 +0100

    Removed debug prints and changed getTile method to return tiles.getOrDefault(tileId, new Tile(tileId)

--- src/main/java/entity/Board.java
+++ src/main/java/entity/Board.java
@@ -7,22 +7,15 @@ public class Board {
   private Map<Integer, Tile> tiles;
 
   public Board() {
-    this.tiles = new HashMap<>();
+    tiles = new HashMap<>();
   }
 
   public void addTile(Tile tile) {
-    // Add debug print to verify tiles are being added correctly
-    System.out.println("Adding tile with ID: " + tile.getTileId());
     tiles.put(tile.getTileId(), tile);
   }
 
   public Tile getTile(int tileId) {
-    // Check if the tile exists before returning it
-    if (!tiles.containsKey(tileId)) {
-      System.out.println("Tile with ID " + tileId + " not found in board map. Map size: " + tiles.size());
-      return null;
-    }
-    return tiles.get(tileId);
+    return tiles.getOrDefault(tileId, new Tile(tileId));
   }
 
   public Map<Integer, Tile> getTiles() {
