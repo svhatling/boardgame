8739eaa4ed354fef07061b32352d6463cc94ae84
sirivha <sirivha@stud.ntnu.no>
2025-03-14 12:22:47 +0100

    Added previously made BoardGameApp code, it had vanished in a merge so retrieved it from an older version.

--- src/main/java/ui/BoardGameApp.java
+++ src/main/java/ui/BoardGameApp.java
@@ -1,5 +1,50 @@
 package ui;
 
-public class BoardGameApp {
+import entity.*;
 
+public class BoardGameApp {
+  private BoardGame game;
+
+  public BoardGameApp() {
+    this.game = new BoardGame();
+  }
+
+  public void start() {
+    // Opprett brettet og terningen i spillet
+    this.game.createBoard();
+    this.game.createDice(2);
+
+    Board board = this.game.getBoard();
+
+
+    // Legg til spillere, hver spiller får sitt startfelt og brett
+    Player player1 = new Player("Mikke", board);
+    Player player2 = new Player("Donald", board);
+    Player player3 = new Player("Langbein", board);
+    Player player4 = new Player("Dolly", board);
+
+    // Add players to the game
+    game.addPlayer(player1);
+    game.addPlayer(player2);
+    game.addPlayer(player3);
+    game.addPlayer(player4);
+
+    game.players();
+
+    int roundNumber = 1;
+    while (!game.isFinished()) {
+      System.out.println("Round number " + roundNumber++);
+      game.playOneRound();
+      if (!game.isFinished()) {
+        game.showPlayerStatus();
+      }
+      System.out.println();
+    }
+    System.out.println("And the winner is: " + game.getWinner().getName());
+  }
+
+  public static void main(String[] args) {
+    BoardGameApp app = new BoardGameApp();
+    app.start();
+  }
 }
