f319a7147e116edb7b053165efb47e28600ace85
sirivha <sirivha@stud.ntnu.no>
2025-03-25 16:01:05 +0100

    Changed how the board is created. Instead of creating with BoardGame, the board is created using BoardGameFactory. Also changed javadocs from norwegian to english

--- src/main/java/ui/BoardGameApp.java
+++ src/main/java/ui/BoardGameApp.java
@@ -1,23 +1,20 @@
 package ui;
 
 import entity.*;
+import factory.BoardGameFactory;
 
 public class BoardGameApp {
   private BoardGame game;
 
   public BoardGameApp() {
-    this.game = new BoardGame();
+    this.game = BoardGameFactory.createLadderGameWithDice(2);
   }
 
   public void start() {
-    // Opprett brettet og terningen i spillet
-    this.game.createBoard("snakesandladders");
-    this.game.createDice(2);
-
     Board board = this.game.getBoard();
 
 
-    // Legg til spillere, hver spiller får sitt startfelt og brett
+    // Add players, each player gets a start tile and board
     Player player1 = new Player("Mikke", board);
     Player player2 = new Player("Donald", board);
     Player player3 = new Player("Langbein", board);
