64b2211ec30aaa2167832a6493a8727f937e8cee
sirivha <sirivha@stud.ntnu.no>
2025-03-25 15:56:34 +0100

    Changed javadocs to be in english

--- src/main/java/entity/LadderAction.java
+++ src/main/java/entity/LadderAction.java
@@ -9,13 +9,13 @@ public class LadderAction implements TileAction {
     if (destinationTileId < 1) {
       throw new IllegalArgumentException("Destination tile ID cannot be negative or zero.");
     }
-    this.destinationTileId = destinationTileId; // Beholder original ID
+    this.destinationTileId = destinationTileId; //Keeps original ID
     this.message = message;
   }
 
   @Override
   public void perform(Player player) {
-    int finalTile = Math.min(destinationTileId, 90); // Begrens spilleren til maks 90
+    int finalTile = Math.min(destinationTileId, 90); // Limits the player to reach max 90
     player.setCurrentTile(player.getBoard().getTile(finalTile));
     System.out.println(player.getName() + " " + message);
   }
