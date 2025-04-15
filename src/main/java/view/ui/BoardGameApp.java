839769d0a5048fa075dd1531d9bd8e85ee5895e7
sirivha <sirivha@stud.ntnu.no>
2025-04-15 12:20:23 +0200

    Refactored BoardGameApp to launch JavaFX application

--- src/main/java/view/ui/BoardGameApp.java
+++ src/main/java/view/ui/BoardGameApp.java
@@ -1,70 +1,35 @@
 package view.ui;
 
-import model.entity.Board;
-import model.entity.BoardGame;
-import model.entity.Player;
-import model.factory.BoardGameFactory;
+import controller.MainViewController;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class BoardGameApp {
-  private BoardGame game;
+public class BoardGameApp extends Application {
 
-  public BoardGameApp() {
-    this.game = BoardGameFactory.createLadderGameWithDice(2);
-  }
+  private final MainViewController controller = new MainViewController();
 
-  public void start() {
-    Board board = this.game.getBoard();
+  @Override
+  public void start(Stage primaryStage) {
+    primaryStage.setTitle("Board Game - Choose Game");
 
+    Button laddersButton = new Button("Ladders & Snakes");
+    Button ludoButton = new Button("Ludo");
 
-    // Add players, each player gets a start tile and board
-    Player player1 = new Player("Mikke", board, "Mouse");
-    Player player2 = new Player("Donald", board, "Duck");
-    Player player3 = new Player("Langbein", board, "Dog");
-    Player player4 = new Player("Dolly", board, "Goose");
+    laddersButton.setOnAction(e -> controller.selectGame("Ladders"));
+    ludoButton.setOnAction(e -> controller.selectGame("Ludo"));
 
-    // Add players to the game
-    game.addPlayer(player1);
-    game.addPlayer(player2);
-    game.addPlayer(player3);
-    game.addPlayer(player4);
+    VBox layout = new VBox(15, laddersButton, ludoButton);
+    layout.setStyle("-fx-padding: 40; -fx-alignment: center;");
 
-    System.out.println("The following players are playing:");
-    for (Player player : game.getPlayers()) {
-      System.out.println("Name: " + player.getName());
-    }
-    System.out.println();
-
-    int roundNumber = 1;
-    Player winner = null;
-    while (winner == null) {
-      System.out.println("Round " + roundNumber++);
-
-      for (Player player : game.getPlayers()) {
-        game.setCurrentPlayer(player);
-        int diceRoll = game.getDice().rollDice();
-
-        System.out.println(player.getName() + " rolled " + diceRoll);
-        int previousTile = player.getCurrentTile().getTileId();
-        player.move(diceRoll);
-        int newTile = player.getCurrentTile().getTileId();
-
-        System.out.println(player.getName() + " moved from tile " + previousTile + " to tile " + newTile);
-
-        if (newTile == board.getTiles().size()) {
-          winner = player;
-          break;
-        }
-      }
-      if (winner == null) {
-        game.showPlayerStatus();
-      }
-      System.out.println();
-    }
-    System.out.println("Winner is " + winner.getName() + "!!<3<3");
+    Scene scene = new Scene(layout, 300, 200);
+    primaryStage.setScene(scene);
+    primaryStage.show();
   }
 
   public static void main(String[] args) {
-    BoardGameApp app = new BoardGameApp();
-    app.start();
+    launch(args);
   }
 }
