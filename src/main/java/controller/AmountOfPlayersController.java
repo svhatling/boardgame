47c0752191662d6c04e938cbb29cfabff71b255d
sirivha <sirivha@stud.ntnu.no>
2025-04-24 14:55:50 +0200

    Reuse primary stage for player selection view and use full-screen for both views. Added javadocs.

--- src/main/java/controller/AmountOfPlayersController.java
+++ src/main/java/controller/AmountOfPlayersController.java
@@ -1,14 +1,28 @@
 package controller;
 
-public class AmountOfPlayersController {
-  private int numberOfPlayers = 2; // Default
+import view.BoardGameObserver;
 
-  public void setNumberOfPlayers(int number) {
-    this.numberOfPlayers = number;
-    System.out.println("Number of players set to: " + number);
+/**
+ * Controller for dealing the number of players selection in the board game.
+ */
+public class AmountOfPlayersController {
+  private final BoardGameObserver observer;
+
+  /**
+   * Constructs a new AmountOfPlayersController.
+   *
+   * @param observer the observer to be notified when the user starts the game
+   */
+  public AmountOfPlayersController(BoardGameObserver observer) {
+    this.observer = observer;
   }
 
-  public int getNumberOfPlayers() {
-    return numberOfPlayers;
+  /**
+   * Notifies the observer that the game should start with the given number of players.
+   *
+   * @param numberOfPlayers the number of players selected to start the game
+   */
+  public void startGame(int numberOfPlayers) {
+    observer.onPlayerCountChosen(numberOfPlayers);
   }
 }
