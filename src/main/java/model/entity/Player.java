1a05a6f56290220eb3a301fc86bb4d116fe973db
sirivha <sirivha@stud.ntnu.no>
2025-05-21 18:48:33 +0200

    Changed javadocs to English and added sendMessage() method

--- src/main/java/model/entity/Player.java
+++ src/main/java/model/entity/Player.java
@@ -6,7 +6,7 @@ public class Player {
   private final Board board;
   private String piece;
 
-  // Konstruktør
+  // Constructor
   public Player(String name, Board board, String piece) {
     this.name = name;
     this.board = board;
@@ -14,31 +14,39 @@ public class Player {
     this.currentTile = board.getTile(1);
   }
 
-  // Metode for å hente navn
+  // Method for getting name
   public String getName() {
     return name;
   }
 
+  //Method for getting piece
   public String getPiece() {return piece;}
 
-  // Henter nåværende tile
+  // Method for getting current tile
   public Tile getCurrentTile() {
     return currentTile;
   }
 
-  // Oppdaterer nåværende tile riktig
+  // Method for setting current tile
   public void setCurrentTile(Tile newTile) {
     this.currentTile = newTile;
   }
 
+  // Method for getting board
   public Board getBoard() {
     return board;
   }
 
+  // Method for moving piece to correct tile
   public void move(int roll) {
     int targetTileId = Math.max(1, Math.min(currentTile.getTileId() + roll, 90));
     Tile targetTile = board.getTile(targetTileId);
     setCurrentTile(targetTile);
     targetTile.landPlayer(this);
+  }
+
+  // TileAction sends message to the user
+  public void sendMessage(String message) {
+    System.out.println(message);
   }
 }
